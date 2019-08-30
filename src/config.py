e519105a17c78140b5062b3319147553dcd979c0
zepcp <vieirapereira@protonmail.com>
2019-08-30 17:10:02 +0100

    remove config

--- src/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-PASSWORD = "0f20a8005b5675e80bf8b6d89d24096c4bb0d0285eb884a74603920a9caecb8a"
-
-VARIABLES = {
-    "VARIABLE": os.getenv("VARIABLE"),
-    "ZEPCP": os.getenv("ZEPCP")
-}
